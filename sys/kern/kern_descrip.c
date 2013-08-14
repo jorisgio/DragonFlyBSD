b40c6f4d53966c99668ea74d23f59528b090bedb
Joris GIOVANNANGELI <joris@giovannangeli.fr>
2013-08-14 18:22:50 +0200

    kernel: removes invalid assertions for ioctls white list

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -157,8 +157,6 @@ filecaps_shallow_copy(const struct filecaps *src, struct filecaps *dst)
 
 	*dst = *src;
 	if (src->fc_ioctls != NULL) {
-		KASSERT(src->fc_ioctls->io_nioctls > 0,
-				("fc_ioctls != NULL, but io_nioctls=%hd", src->fc_ioctls->io_nioctls));
 		ioctlshold(src->fc_ioctls);
 	}
 }
@@ -280,9 +278,6 @@ filecaps_validate(const struct filecaps *fcaps, const char *func)
 			("%s: invalid fcntls %u : %u, %u, %u", func, fcaps->fc_fcntls, CAP_FCNTL_ALL, ~CAP_FCNTL_ALL, fcaps->fc_fcntls & ~CAP_FCNTL_ALL));
 	KASSERT(fcaps->fc_fcntls == 0 || (fcaps->fc_rights & CAP_FCNTL) != 0,
 			("%s: fcntls without CAP_FCNTL %lu %p %u", func, fcaps->fc_rights, fcaps->fc_ioctls, fcaps->fc_fcntls ));
-	KASSERT((fcaps->fc_ioctls != NULL && fcaps->fc_ioctls->io_nioctls > 0)
-			|| fcaps->fc_ioctls == NULL,
-			("%s: invalid ioctls", func));
 	KASSERT(fcaps->fc_ioctls != NULL || (fcaps->fc_rights & CAP_IOCTL) != 0,
 			("%s: ioctls without CAP_IOCTL", func));
 }
